76c4d681fc1dfa1214dbc6a832a5f0b858b6fc34
KeylorCalderon <43432543+KeylorCalderon@users.noreply.github.com>
2022-01-11 20:19:51 -0600

    Gestionar descuentos, agregarlos y eliminarlos

--- agregarDescuento.php
+++ agregarDescuento.php
@@ -1,5 +1,5 @@
 <?php 
-  if (isset($_POST['nombre'])) {
+  if (isset($_POST['porcentaje'])) {
     $extensiones = array(0=>'image/jpg',1=>'image/jpeg',2=>'image/png');
     $max_tamanyo = 1024 * 1024 * 16;
     $ruta_indexphp = dirname(realpath(__FILE__));
@@ -12,25 +12,24 @@
               echo 'Fichero guardado con éxito';
                   include "includes/Conexion.php";
                   $conn=conectar();
-                  $nombre = $_POST['nombre'];
-                  $select = $_POST['select'];
-                  $precio = $_POST['precio'];
-                  $codigo = $_POST['codigo'];
-                  $descripcionVideojuego=$_POST['DescripcionV'];
-                  $sql = "INSERT INTO Videojuego(nombre, codigo, plataformaID, precio, imagen,descripcion, activo)
-                  VALUES  ('$nombre', '$codigo','$select', '$precio' ,'X','$descripcionVideojuego', 1);";
+                  $porcentaje = $_POST['porcentaje'];
+                  $juego = $_POST['select'];
+                  $sql = "INSERT INTO Descuento(videojuegoID, descuento, imagen)
+                  VALUES  ('$juego', '$porcentaje','X');";
                     try {
                       mysqli_query($conn, $sql);
                       $ultimo_id = mysqli_insert_id($conn); 
-                      $nombreFichero='img/'.$nombre.$ultimo_id.'.png';
+                      $nombreFichero='img/Descuento'.$ultimo_id.'.png';
                       $ruta_nuevo_destino = $ruta_indexphp . '/' . $nombreFichero;
                       if(move_uploaded_file ($ruta_fichero_origen, $ruta_nuevo_destino)){
-                        $sql = "UPDATE Videojuego SET imagen='$nombreFichero' WHERE ID='$ultimo_id'";
+                        $sql = "UPDATE Descuento SET imagen='$nombreFichero' WHERE ID='$ultimo_id'";
                         mysqli_query($conn, $sql);
                         mysqli_close($conn);
-                        header("Location: GestionarProductos.php");
+                        header("Location: GestionarDescuentos.php");
+                      }else{
+                        echo "<div>Error al cargar la imagen '$ruta_nuevo_destino'</div>";
                       }
-                      echo "<div>Error al cargar la imagen '$ruta_nuevo_destino'</div>";
+                      
                   } catch (Exception $e) {
                       echo 'Error al cargar datos: ',  $e->getMessage(), "\n";
                   }
@@ -50,11 +49,11 @@
   <div class="wrapper">
     <div class="carrito">
         <div class="col-md-12 order-md-1">
-          <h4 class="mb-3"><b>¡Agrega un videojuego al catálogo!</b></h4>
+          <h4 class="mb-3"><b>¡Agrega un descuento a un videojuego!</b></h4>
           <form class="cont-articulo" action="#" method = "post" enctype="multipart/form-data">
               <div class="row">
-                <div class="col-md-12 mb-3"> <label for="nombre">Nombre del videojuego</label>
-                  <input type="text" class="form-control" id="nombre" placeholder="Por ejemplo: Final Fantasy X" name="nombre" required="">
+                <div class="col-md-12 mb-3"> <label for="nombre">Porcentaje de descuento</label>
+                  <input type="number" min="1" max="99" class="form-control" id="porcentaje" placeholder="Por ejemplo: 1-90" name="porcentaje" required="">
                 </div>
               </div>
               <div class="row">
@@ -63,41 +62,37 @@
                 </div>
               </div>
                 <div class="row">
-                  <div class="col-md-12 mb-3"> <label for="plataforma">Plataforma</label> 
+                  <div class="col-md-12 mb-3"> <label for="plataforma">Videojuego</label> 
                     <select class="form-control" id="plataforma" required="" name = "select">
                         <option value="">Elige...</option>
-                        <option value="1">Steam</option>
-                        <option value="2">Epic</option>
-                        <option value="3">Android</option>
-                        <option value="4">PlayStation 4</option>
-                        <option value="5">PlayStation 5</option>
-                        <option value="6">Xbox One</option>
-                        <option value="7">Xbox series</option>
-                        <option value="8">Nintendo Switch</option>
+                        <?php
+                            $conn=conectar();
+                            $result=mysqli_query($conn, "SELECT V.ID, V.nombre, P.nombre AS plataforma
+                                                        FROM Videojuego V, Plataforma P
+                                                        WHERE V.activo=1 AND P.ID=V.plataformaID
+                                                        AND V.ID NOT IN
+                                                        (
+                                                            SELECT D.videojuegoID
+                                                            FROM Descuento D
+                                                        )
+                                                        ORDER BY V.ID ASC");                     
+                            while($row=mysqli_fetch_assoc($result)){
+                                $ID=$row['ID'];
+                                $nombre=$row['nombre'];
+                                $plataforma=$row['plataforma'];
+                                echo "<option value='$ID'>$nombre-$plataforma</option>";
+                            }
+                            mysqli_close($conn);
+                        ?>
+                        
                       </select>
                   </div>
               </div>
 
-            <div class="row">
-            <div> 
-                <label for="precio">Precio del videojuego</label>
-                <input type="number" class="form-control" id="precio"  name="precio" required="">
-            </div>
-            </div>
-            <div class="row">
-            <div> 
-                <label for="Codigo">Codigo del videojuego</label>
-                <input type="text" class="form-control" id="Codigo"  name="codigo" required="">
-            </div>
-            <div class="row"> 
-              <div>
-                <label for="DescripcionV">Descripción del videojuego</label>
-                <input type="text" class="form-control" id="DescripcionV"  name="DescripcionV" required="">
-              </div>
-            </div>
+            
             </div>
             <div class="rowbtn">
-            <button type="submit" class="btn-carrito">¡Agregalo ahora!</button>
+            <button type="submit" class="btn-carrito">Agregar</button>
             </div>
           </form>
         </div>
