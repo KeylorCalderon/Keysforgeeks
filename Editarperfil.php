ba535eec002ea8fa4af77b67dcb9ca66c3c94c64
KeylorCalderon <43432543+KeylorCalderon@users.noreply.github.com>
2022-01-11 20:40:16 -0600

    Cambios menores

--- Editarperfil.php
+++ Editarperfil.php
@@ -14,7 +14,26 @@
                     <form class = "cont-perfil" action="#" method = "post" name= "">
                         <div>
 
-                            <?php echo $_SESSION['ID']; ?>
+                            <?php 
+                                $clienteID=$_SESSION['ID'];
+                                $conn=conectar();
+                                $result=mysqli_query($conn, "SELECT U.ID, C.direccion, C.movil, U.email, U.contrasena FROM Usuario U, Cliente C WHERE U.ID=C.usuarioID AND C.ID='$clienteID'");                     
+                                $row=mysqli_fetch_assoc($result);
+                                $usuarioID=$row['ID'];
+                                $direccion=$row['direccion'];
+                                $movil=$row['movil'];
+                                $email=$row['email'];
+                                $contrasena=$row['contrasena'];
+
+                                mysqli_close($conn);
+
+                                echo "<div>ID cliente: '$clienteID'</div>
+                                    <div>ID usuario: '$usuarioID'</div>
+                                    <div>ID direccion: '$direccion'</div>
+                                    <div>ID movil: '$movil'</div>
+                                    <div>ID email: '$email'</div>
+                                    <div>ID contrasena: '$contrasena'</div>";
+                            ?>
                         </div>
 
 
