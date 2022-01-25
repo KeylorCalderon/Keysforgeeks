3f578bfc4fd683d7af533a532196b6e09c100fa7
KeylorCalderon <43432543+KeylorCalderon@users.noreply.github.com>
2022-01-24 21:51:35 -0600

    Visualización de las facturas en detalle
    #50

--- GraficosFacturas.php
+++ GraficosFacturas.php
@@ -26,6 +26,90 @@
                     </div>
                 </div>  
                 <div id="chartContainer" style="height: 100%; width: 100%;"></div>
+                <?php
+                    if (isset($_POST['fechaFin'])) {
+                        
+                        //include "includes/Conexion.php";
+                        $connection=conectar();
+                        $fechaInicio = $_POST['fechaInicio'];
+                        $fechaFin = $_POST['fechaFin'];
+                        $tipoBusqueda=$_POST["tipoBusqueda"];
+                        $fechaInicioAux=$fechaInicio;
+
+                        //echo  "<div>Tipo busqueda: $tipoBusqueda</div>";
+
+                        $dataPoints = array(
+                        );
+
+                        if($fechaFin<$fechaInicio){
+                            echo "<div>ERROR: la fecha de inicio debe ser anterior a la fecha de fin</div>";
+                        }else{
+                            while($fechaInicio<=$fechaFin){
+                                //echo "<div>$fechaInicio</div></br>";
+
+                                $cantFacturas=0;
+                                $result=null; 
+
+                                switch ($tipoBusqueda) {
+                                    case 1:
+                                        $result=mysqli_query($connection, "SELECT * FROM Factura WHERE Factura.fecha='$fechaInicio'"); 
+                                        break;
+                                    case 2:
+                                        $result=mysqli_query($connection, "SELECT * FROM Factura WHERE Factura.fecha='$fechaInicio' AND Factura.estado=1"); 
+                                        break;
+                                    case 3:
+                                        $result=mysqli_query($connection, "SELECT * FROM Factura WHERE Factura.fecha='$fechaInicio' AND Factura.estado=0"); 
+                                        break;
+                                }
+                                    
+                                while($row=mysqli_fetch_assoc($result)){
+                                    $cantFacturas+=1;
+                                    //echo "<div>$fechaInicio:=$cantFacturas</div></br>";
+                                }
+
+                                array_push($dataPoints, array("label"=> $fechaInicio, "y"=> $cantFacturas));
+
+                                $fechaInicio=date("Y-m-d",strtotime($fechaInicio."+ 1 days"));
+                            }
+
+                            echo "<script>
+                            window.onload = function () {
+                            
+                            var chart = new CanvasJS.Chart('chartContainer', {
+                                animationEnabled: false, //si cambia su valor a true se verá una animación
+                                exportEnabled: true, //permite guardar exportar el gráfico como JPEG/PNG
+                                theme: 'light1', // 'light1', 'light2', 'dark1', 'dark2'
+                                title:{
+                                    text: 'Gráfico'
+                                },
+                                axisX:{      
+                                    intervalType: 'day',
+                                    valueFormatString: 'YYYY-MM-DD'
+                                },
+                                axisY:{
+                                    includeZero: true
+                                },
+                                data: [{
+                                    type: 'column', //para cambiar el tipo: bar, line, area, pie, etc
+                                    indexLabel: '{y}', //Muestra el valor de 'y' en todos los puntos
+                                    indexLabelFontColor: '#5A5757',
+                                    indexLabelPlacement: 'outside',   
+                                    dataPoints:";
+                            echo json_encode($dataPoints);
+                            echo "
+                                }]
+                            });
+                            chart.render();
+                            
+                            }
+                            </script>";
+
+                            echo "<form class='formulario' method='post' action='verFacturasDetalle.php?fechaInicio=$fechaInicioAux&fechaFin=$fechaFin&tipo=$tipoBusqueda' name='verDetalle' id='verDetalle'>
+                                    <button type='submit' class='botonLogin' name='verEnDetalle' value='verEnDetalle'>Ver facturas</button>
+                                </form>";
+                        }
+                    }
+                ?>
             </div>      
         </div> 
     </div>  
@@ -38,97 +122,4 @@
      </body>
      </html> 
     
-     <?php
-    if (isset($_POST['fechaFin'])) {
-        
-        //include "includes/Conexion.php";
-        $connection=conectar();
-        $fechaInicio = $_POST['fechaInicio'];
-        $fechaFin = $_POST['fechaFin'];
-        $tipoBusqueda=$_POST["tipoBusqueda"];
-
-        //echo  "<div>Tipo busqueda: $tipoBusqueda</div>";
-
-        $dataPoints = array(
-        );
-
-        if($fechaFin<$fechaInicio){
-            echo "<div>ERROR: la fecha de inicio debe ser anterior a la fecha de fin</div>";
-        }else{
-            while($fechaInicio<=$fechaFin){
-                //echo "<div>$fechaInicio</div></br>";
-
-                $cantFacturas=0;
-                $result=null; 
-
-                switch ($tipoBusqueda) {
-                    case 1:
-                        $result=mysqli_query($connection, "SELECT * FROM Factura WHERE Factura.fecha='$fechaInicio'"); 
-                        break;
-                    case 2:
-                        $result=mysqli_query($connection, "SELECT * FROM Factura WHERE Factura.fecha='$fechaInicio' AND Factura.estado=1"); 
-                        break;
-                    case 3:
-                        $result=mysqli_query($connection, "SELECT * FROM Factura WHERE Factura.fecha='$fechaInicio' AND Factura.estado=0"); 
-                        break;
-                }
-                    
-                while($row=mysqli_fetch_assoc($result)){
-                    $cantFacturas+=1;
-                    //echo "<div>$fechaInicio:=$cantFacturas</div></br>";
-                }
-
-                array_push($dataPoints, array("label"=> $fechaInicio, "y"=> $cantFacturas));
-
-                $fechaInicio=date("Y-m-d",strtotime($fechaInicio."+ 1 days"));
-            }
-
-            echo "<script>
-            window.onload = function () {
-             
-            var chart = new CanvasJS.Chart('chartContainer', {
-                animationEnabled: false, //si cambia su valor a true se verá una animación
-                exportEnabled: true, //permite guardar exportar el gráfico como JPEG/PNG
-                theme: 'light1', // 'light1', 'light2', 'dark1', 'dark2'
-                title:{
-                    text: 'Gráfico'
-                },
-                axisX:{      
-                    intervalType: 'day',
-                    valueFormatString: 'YYYY-MM-DD'
-                },
-                axisY:{
-                    includeZero: true
-                },
-                data: [{
-                    type: 'column', //para cambiar el tipo: bar, line, area, pie, etc
-                    indexLabel: '{y}', //Muestra el valor de 'y' en todos los puntos
-                    indexLabelFontColor: '#5A5757',
-                    indexLabelPlacement: 'outside',   
-                    dataPoints:";
-            echo json_encode($dataPoints);
-            echo "
-                }]
-            });
-            chart.render();
-             
-            }
-            </script>";
-        }
-
-        /*$result=mysqli_query($connection, "SELECT * FROM Factura WHERE Factura.fecha
-                                           BETWEEN CAST('$fechaInicio' AS DATE) AND
-                                           CAST('$fechaFin' AS DATE);");                     
-        while($row=mysqli_fetch_assoc($result)){
-                echo $row["ID"];
-                echo "-";
-        }
-
-        mysqli_close($connection);*/
-    }
-    //SELECT * FROM Factura WHERE Factura.fecha BETWEEN CAST('2021-09-01' AS DATE) AND CAST('2021-11-30' AS DATE);
-    //include "includes/Conexion.php";
-    //$conn=conectar();
-    //%graphData= array();
-         
-?>
+    
---
+++ verFacturasDetalle.php
@@ -0,0 +1,92 @@
+<?php
+        include "includes/sesionInicio.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<?php
+        include "includes/Encabezado.php";
+?>
+<body>
+    <div class="wrapper">        
+        <main>
+            <?php 
+                $fechaInicio=$_GET['fechaInicio'];
+                $fechaFin=$_GET['fechaFin'];
+                $tipoBusqueda=$_GET['tipo'];
+                //echo "<div>$fechaInicio---$fechaFin</div></br>";
+                $conn=conectar();
+                $result=null; 
+
+                switch ($tipoBusqueda) {
+                    case 1:
+                        $result=mysqli_query($conn,"SELECT U.email, F.ID, F.fecha, F.subtotal, F.estado
+                                                    FROM Factura F, Cliente C, Usuario U
+                                                    WHERE F.carritoID=C.ID AND C.ID=U.ID AND F.fecha
+                                                    BETWEEN CAST('$fechaInicio' AS DATE) AND
+                                                    CAST('$fechaFin' AS DATE)
+                                                    ORDER BY F.fecha ASC"); 
+                        break;
+                    case 2:
+                        $result=mysqli_query($conn,"SELECT U.email, F.ID, F.fecha, F.subtotal, F.estado
+                                                    FROM Factura F, Cliente C, Usuario U
+                                                    WHERE F.carritoID=C.ID AND C.ID=U.ID AND F.estado=1 AND F.fecha
+                                                    BETWEEN CAST('$fechaInicio' AS DATE) AND
+                                                    CAST('$fechaFin' AS DATE)
+                                                    ORDER BY F.fecha ASC"); 
+                        break;
+                    case 3:
+                        $result=mysqli_query($conn,"SELECT U.email, F.ID, F.fecha, F.subtotal, F.estado
+                                                    FROM Factura F, Cliente C, Usuario U
+                                                    WHERE F.carritoID=C.ID AND C.ID=U.ID AND F.estado=0 AND F.fecha
+                                                    BETWEEN CAST('$fechaInicio' AS DATE) AND
+                                                    CAST('$fechaFin' AS DATE)
+                                                    ORDER BY F.fecha ASC"); 
+                        break;
+                }
+         
+                echo "<div class= 'carrito'>";
+                while($row=mysqli_fetch_assoc($result)){
+                    $facturaID=$row['ID'];
+                    $fecha=$row['fecha'];
+                    $subtotal=$row['subtotal'];
+                    $nombreUsuario=$row['email'];
+                    echo "<h2 class='subtitulos'>Fecha: $fecha
+                            <div>Cliente: $nombreUsuario</div>";
+
+                    $estado = $row['estado'];
+                    if($estado==1){
+                        echo "<div>ACTIVA</div>";
+                    }else{
+                        echo "<div>CANCELADA</div>";
+                    }
+                    echo "</h2>
+                    <div class= 'cont-compras'>";  
+                    $result2=mysqli_query($conn,"SELECT FD.nombre, FD.precio
+                                                FROM FacturaDetalle FD
+                                                WHERE FD.facturaID='$facturaID'");
+                    while($row2=mysqli_fetch_assoc($result2)){
+                        $nombre=$row2['nombre'];
+                        $precio=$row2['precio'];
+                        echo "<p>Articulo: $nombre</p>"; 
+                        echo "<p>● Precio: ₡ $precio</p>"; 
+                                          
+                    }
+                    echo "<p class = 'text-fact-carrito'>Total: ₡ $subtotal</p></div>";      
+                }
+                echo "</div>";
+                mysqli_close($conn);
+            ?>
+        </main>
+
+    </div>
+
+    <?php
+        include "includes/PiePagina.php";
+    ?>
+</body>
+
+</html>
